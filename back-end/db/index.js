fc8973c72c553b3e682547f7bfaae49af7cef707
Alex Emanuel Amico <emanuelamicoo@gmail.com>
2021-08-20 22:58:58 -0300

    deprecation warnings fixes

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
 
+//Config for deprecation warnings
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 const username = process.env.DB_USER
 const password = process.env.DB_PASSWORD
 const hostname = process.env.DB_HOST
